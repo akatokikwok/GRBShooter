2a18c4c807c170aa2826d9e03633be77196f1cad
DESKTOP-ASKYR1M\Askyr1m <akatokikwok@outlook.com>
2024-05-26 19:30:22 +0800

    [init]: 初始化GRBFPS项目，现在为1个空项目.

---
+++ Source/GRBFPS/GRBFPSGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GRBFPSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRBFPS_API AGRBFPSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
