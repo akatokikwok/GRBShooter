68ff222d781350ca8fb5d363df48613628c3dd20
DESKTOP-ASKYR1M\Askyr1m <akatokikwok@outlook.com>
2024-05-26 21:38:26 +0800

    [config]: 打开一些GAS插件。

--- Source/GRBFPS/GRBFPS.Build.cs
+++ Source/GRBFPS/GRBFPS.Build.cs
@@ -7,18 +7,22 @@ public class GRBFPS : ModuleRules
 	public GRBFPS(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
+
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NetCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { 
+		PrivateDependencyModuleNames.AddRange(new string[]
+		{
+			"Slate",
+			"SlateCore",
 			"GameplayAbilities",
 			"GameplayTags",
-			"GameplayTasks", 
+			"GameplayTasks",
+			"Paper2D"
 		});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
